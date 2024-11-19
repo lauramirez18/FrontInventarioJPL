300bfe92e7bf41cfca30e622bb048df07f91bae2
Your Name <you@example.com>
2024-11-19 07:03:25 -0500

    usuarios

--- src/views/Usuarios.vue
+++ src/views/Usuarios.vue
@@ -3,8 +3,7 @@
         <h4 class="text-center text-weight-bold">Usuarios</h4>
         <hr>
 
-       
-        <q-btn label="+ Registrar"  @click="abrirFormulario" class="q-mb-md" />
+        <q-btn label="+ Registrar" @click="abrirFormulario" class="q-mb-md" />
 
         <q-table class="tabla-clientes" :rows="rows" :columns="columns" row-key="name">
             <template v-slot:header="props">
@@ -24,7 +23,7 @@
 
             <template v-slot:body-cell-opciones="props">
                 <q-td :props="props" class="tabla-cell opciones">
-                    <q-btn icon="edit" color="primary" flat @click="editarProveedor(props.row)" class="q-mr-sm" />
+                    <q-btn icon="edit" color="primary" flat @click="editarUsuario(props.row)" class="q-mr-sm" />
                     <q-btn :icon="props.row.estado === 1 ? 'remove_circle' : 'check_circle'" color="negative" flat
                         @click="mostrarModalConfirmacion(props.row)" />
                 </q-td>
@@ -44,6 +43,7 @@
             </q-card>
         </q-dialog>
 
+
         <q-dialog v-model="modalAgregarUsuario" persistent>
             <q-card>
                 <q-card-section>
@@ -56,16 +56,35 @@
 
                 <q-card-actions>
                     <q-btn label="Cancelar" color="secondary" flat @click="cerrarFormulario" />
-                    <q-btn label="Guardar" color="primary" flat @click="guardarUsuario" />
+                    <q-btn label="Guardar" color="primary" flat @click="postUsuario" />
                 </q-card-actions>
             </q-card>
         </q-dialog>
+
+    
+        <q-dialog v-model="modalEditarUsuario" persistent>
+            <q-card>
+                <q-card-section>
+                    <div class="text-h6">Editar Usuario</div>
+                    <q-input v-model="usuarioEditar.nombre" label="Nombre" filled />
+                    <q-input v-model="usuarioEditar.email" label="Email" filled />
+                    <q-input type="password" v-model="usuarioEditar.password" label="Contraseña" filled />
+                    <q-input v-model="usuarioEditar.estado" label="Estado" filled type="number" min="0" max="1" />
+                </q-card-section>
+
+                <q-card-actions>
+                    <q-btn label="Cancelar" color="secondary" flat @click="cerrarModalEditar" />
+                    <q-btn label="Guardar Cambios" color="primary" flat @click="putUsuarios" />
+                </q-card-actions>
+            </q-card>
+        </q-dialog>
+
     </q-page>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
-import { getData, postData } from '../services/apiClient.js'  
+import { getData, postData, putData } from '../services/apiClient.js'
 import { useAuthStore } from '../store/useAuth.js'
 
 const columns = ref([
@@ -102,22 +121,33 @@ const columns = ref([
 ])
 
 const rows = ref([]);
-const modalConfirmarEstado = ref(false); 
-const modalAgregarUsuario = ref(false);   
+const modalConfirmarEstado = ref(false);
+const modalAgregarUsuario = ref(false);
+const modalEditarUsuario = ref(false);
+
 const nuevoUsuario = ref({
     nombre: '',
     email: '',
     password: '',
-    estado: 1,  
+    estado: 1,
 });
+
+const usuarioEditar = ref({
+    _id: '',
+    nombre: '',
+    email: '',
+    password: '',
+    estado: 1,
+});
+
 const usuarioSeleccionado = ref(null);
 const authStore = useAuthStore()
 
 onMounted(async () => {
-    await getDataFromApi()
+    await getUsuarios()
 })
 
-async function getDataFromApi() {
+async function getUsuarios() {
     const token = authStore.getToken();
     console.log("Token recuperado del store", token)
     if (!token) {
@@ -138,17 +168,17 @@ async function getDataFromApi() {
 }
 
 const mostrarModalConfirmacion = (usuario) => {
-    usuarioSeleccionado.value = usuario;  
-    modalConfirmarEstado.value = true;   
+    usuarioSeleccionado.value = usuario;
+    modalConfirmarEstado.value = true;
 }
 
 const abrirFormulario = () => {
-    modalAgregarUsuario.value = true; 
+    modalAgregarUsuario.value = true;
 }
 
 const cerrarFormulario = () => {
-    modalAgregarUsuario.value = false; 
-    resetFormulario();  
+    modalAgregarUsuario.value = false;
+    resetFormulario();
 }
 
 const resetFormulario = () => {
@@ -156,22 +186,39 @@ const resetFormulario = () => {
         nombre: '',
         email: '',
         password: '',
-        estado: 1,  
+        estado: 1,
     };
 }
 
-const guardarUsuario = async () => {
+const postUsuario = async () => {
     try {
-        
         const response = await postData('registro', nuevoUsuario.value);
         console.log('Usuario creado con éxito', response);
-
-        
         modalAgregarUsuario.value = false;
-        await getDataFromApi();
-        resetFormulario(); 
+        await getUsuarios();
+        resetFormulario();
     } catch (error) {
         console.log('Error al crear usuario:', error.response ? error.response.data : error);
+    }
+}
+
+const editarUsuario = (usuario) => {
+    usuarioEditar.value = usuario;
+    modalEditarUsuario.value = true;
+}
+
+const cerrarModalEditar = () => {
+    modalEditarUsuario.value = false;
+}
+
+const putUsuarios = async () => {
+    try {
+        const response = await putData(`usuarios/${usuarioEditar.value._id}`, usuarioEditar.value);
+        console.log('Usuario editado con éxito', response);
+        modalEditarUsuario.value = false;
+        await getUsuarios();
+    } catch (error) {
+        console.log('Error al editar el usuario:', error.response ? error.response.data : error);
     }
 }
 </script>
