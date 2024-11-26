714123bd4ff8171ca28818a3af140961fcb7fb99
Your Name <you@example.com>
2024-11-26 11:16:57 -0500

    CAMBIO DE URL

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,12 +1,12 @@
 import Axios from 'axios';
 
 const token = JSON.parse(localStorage.getItem('auth'));  
- console.log(token.token);
+
 
 
 const apiClient = Axios.create({
     /* baseURL: 'http://localhost:2999/api', */
-    baseURL: 'https://inventario-jpl.onrender.com/api',
+    baseURL: 'https://inventariojpl.onrender.com/api',
     headers: {
         "x-token": token.token
     }
