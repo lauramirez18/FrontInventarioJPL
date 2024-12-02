460d63d9be629a194cf4337219360b43a6ffbfca
Your Name <you@example.com>
2024-12-02 08:35:41 -0500

    verificacion token

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,6 +1,11 @@
 import Axios from 'axios';
 
-const token = JSON.parse(localStorage.getItem('auth'));  
+const token = JSON.parse(localStorage.getItem('auth')); 
+if (token) {
+    console.log("Token encontrado:", token);
+} else {
+    console.log("Token no encontrado en localStorage");
+} 
 
 console.log("mitoken",token);
 
